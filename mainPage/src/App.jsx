eacd1e0bf41e818a60f94a0e4e927404e80e2e08
TomiHuspenina <thuspenina@gmail.com>
2025-03-23 17:17:38 -0300

    animations added

--- mainPage/src/App.jsx
+++ mainPage/src/App.jsx
@@ -1,41 +1,44 @@
 import React from "react";
 import "./style.css";
-import { FaWhatsapp } from "react-icons/fa"; 
+import { FaWhatsapp, FaUniversity, FaClock } from "react-icons/fa"; 
 
 export default function App() {
-
     const mensaje = "Hola, me gustaría crear un usuario, me llamo...";
-    const telefono = 5493517437218
+    const telefono = 5493517437218;
 
     return (
         <div className="mainContent">
-           <div className="title">
+            <div className="title">
+                <img src="https://www.gifss.com/economia/juego/images/ruleta-06.gif" alt="GIF ruleta" />
                 <h2>PARA CREAR TU CUENTA ENVÍANOS UN WHATSAPP</h2>
                 <h2>HAZ CLICK EN EL BOTÓN DE ABAJO</h2>
-           </div>
-           <div className="content">
-            <button>
-                <a
-                    href={`https://wa.me/${telefono}?text=${encodeURIComponent(mensaje)}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="whatsapp-button"
-                >
-                    <FaWhatsapp className="whatsapp-icon" />
-                    CREAR CUENTA
-                </a>  
-            </button>
-            <p>MÍNIMO DE CARGA $3000</p>
-
-           </div>
-           <div className="advertisements">
-                <p>SE ACEPTA TRANSFERENCIAS</p>
-                <p>ATENCIÓN 24 HORAS, TODOS LOS DÍAS</p>
-                <p>PROHIBIDO EL INGRESO A MENORES DE 18 AÑOS</p>
-                <p>JUGAR DE FORMA MODERADA</p>
-           </div>
+            </div>
+            <div className="content">
+                <button className="whatsapp-button-container">
+                    <a
+                        href={`https://wa.me/${telefono}?text=${encodeURIComponent(mensaje)}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="whatsapp-button"
+                    >
+                        <FaWhatsapp className="whatsapp-icon" />
+                        CREAR CUENTA
+                    </a>  
+                </button>
+                <p className="animated-text">MÍNIMO DE CARGA $3000</p>
+            </div>
+            <div className="info-box message1-box">
+                <p>
+                    <FaUniversity className="icon" /> SE ACEPTA TRANSFERENCIAS
+                </p>
+            </div>
+            <div className="info-box message2-box">
+                <p>
+                    <FaClock className="icon" /> ATENCIÓN 24 HORAS, TODOS LOS DÍAS
+                </p>
+            </div>
+            <p>PROHIBIDO EL INGRESO A MENORES DE 18 AÑOS</p>
+            <p>JUGAR DE FORMA MODERADA</p>
         </div>
-      
     );
-  }
-  
+}
